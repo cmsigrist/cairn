0da28e4d24695b5d5a95a8f7a415815858e0d0c1
cmsigrist <c.bergersigrist@gmail.com>
2024-07-21 18:16:57 +0200

    Set F5 to generate a new character on generate character page

--- src/pages/GenerateCharacter.tsx
+++ src/pages/GenerateCharacter.tsx
@@ -1,72 +1,26 @@
-import { FC } from "react";
-import { AllBackgrounds, Background } from "../types/backgrounds";
-import { Bonds } from "../types/bonds";
-import { Character } from "../types/character";
-import { Die } from "../types/die";
-import { Omens } from "../types/omens";
-import { Traits } from "../types/traits";
-import { rand, rollRand } from "../utils/roll";
-import { Attributes } from "../types/attributes";
+import { FC, useEffect, useState } from "react";
+import { generateCharacter } from "../utils/roll";
 import GeneratedCharacter from "./GeneratedCharacter";
 import { Stack } from "@mui/material";
 
 const GenerateCharacter: FC = () => {
-  const generateCharacter = () => {
-    const backgroundOptions = AllBackgrounds[rand(Die.d20)];
-    const background: Background = {
-      name: backgroundOptions.name,
-      description: backgroundOptions.description,
-      playerName: backgroundOptions.playerNames[rand(Die.d10, true)],
-      startingGears: backgroundOptions.startingGears,
-      startingGold: rollRand(
-        backgroundOptions.startingGold.die,
-        backgroundOptions.startingGold.number
-      ),
-      tables: [
-        {
-          question: backgroundOptions.tables[0].question,
-          answer: backgroundOptions.tables[0].answers[rand(Die.d6, true)],
-        },
-        {
-          question: backgroundOptions.tables[1].question,
-          answer: backgroundOptions.tables[1].answers[rand(Die.d6, true)],
-        },
-      ],
-      img: backgroundOptions.img,
-    };
-    const attributes: Attributes = {
-      strength: rand(Die.d6),
-      dexterity: rand(Die.d6),
-      willpower: rand(Die.d6),
-    };
-    const hitProtection = rand(Die.d6);
-    const traits: Traits = {
-      physique: rand(Die.d10),
-      skin: rand(Die.d10),
-      hair: rand(Die.d10),
-      face: rand(Die.d10),
-      speech: rand(Die.d10),
-      clothing: rand(Die.d10),
-      virtue: rand(Die.d10),
-      vice: rand(Die.d10),
-    };
-    const bond = Bonds[rand(Die.d20, true)];
-    const age = rand(Die.d20) + rand(Die.d20) + 10;
-    const omen = Omens[rand(Die.d20, true)];
-
-    return new Character(
-      background,
-      attributes,
-      hitProtection,
-      traits,
-      bond,
-      age,
-      omen
-    );
+  const [character, setCharacter] = useState(generateCharacter());
+  const handleKeyDown = (event: any) => {
+    if (event.key === "F5") {
+      event.preventDefault();
+      setCharacter(generateCharacter())
+    }
   };
 
-  const character = generateCharacter();
+  useEffect(() => {
+    // Add the event listener
+    window.addEventListener("keydown", handleKeyDown);
 
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <Stack
       direction="row"
